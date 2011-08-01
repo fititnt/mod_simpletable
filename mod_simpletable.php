6bf46844b1bed287d6bf04c6b7709f7b3a342827
fititnt <emerson@webdesign.eng.br>
2011-08-01 02:23:12 -0300

    1.0beta1 - Ready for public test

--- mod_simpletable.php
+++ mod_simpletable.php
@@ -2,7 +2,7 @@
 /*
  * @package         Mod_simpletable
  * @author          Emerson Rocha Luiz ( emerson@webdesign.eng.br - @fititnt -  http://fititnt.org )
- * @copyright       Copyright (C) 2011 Joomla! Coders Brazil ( @JCoderBR - http://jcoder.org )
+ * @copyright       Copyright (C) 2005 - 2011 Webdesign Assessoria em Tecnologia da Informacao.
  * @license         GPL3
  */
 // no direct access
@@ -10,12 +10,37 @@ defined('_JEXEC') or die;
 
 // Include helper.php once
 require_once dirname(__FILE__).'/helper.php';
-require JModuleHelper::getLayoutPath('mod_simpletable', $params->get('layout', 'default'));
 
-if( $params->get('loadcss', 1) == 1){
-    JHTML::stylesheet('werandonmessage.css', '/modules/mod_werandonmessage/');
+$modbefore = htmlspecialchars_decode(($params->get('modbefore','')));
+$modafter = htmlspecialchars_decode(($params->get('modafter','')));
+
+$tableclass = htmlspecialchars($params->get('tableclass', ''));
+if($tableclass != ''){
+    $tableclass = ' class = "'.$tableclass.'"';
+} else {
+    $tableclass = '';
+}
+$tablerowprefix = htmlspecialchars($params->get('tablerowprefix', 'cat-list-row'));
+if( !$tablerowprefix ){
+    $tablerowprefix = '';
 }
 
-$moduleclass_sfx = htmlspecialchars($params->get('moduleclass_sfx'));
+$nitems = count($table);
 
-$table = runQuery( &$params );//Must be by reference
+$st = new SimpleTable;
+if($params->get('querymode', '0') == 0){
+    $table = $st->runQuery( &$params );//Must be by reference
+} else {
+    $table = $st->runQueryRaw( &$params );//Must be by reference
+}
+
+if( $params->get('debug', 0)== 1){
+    echo '<pre>';
+    echo 'Table Object';
+    print_r($table);
+    echo '<hr /> Module Parameters';
+    print_r($params);
+    echo '</pre>'; 
+}
+
+require JModuleHelper::getLayoutPath('mod_simpletable', $params->get('layout', 'default'));
--- tmpl/default.php
+++ tmpl/default.php
@@ -2,53 +2,40 @@
 /*
  * @package         Mod_simpletable
  * @author          Emerson Rocha Luiz ( emerson@webdesign.eng.br - @fititnt -  http://fititnt.org )
- * @copyright       Copyright (C) 2011 Joomla! Coders Brazil ( @JCoderBR - http://jcoder.org )
+ * @copyright       Copyright (C) 2005 - 2011 Webdesign Assessoria em Tecnologia da Informacao.
  * @license         GPL3
  */
 // no direct access
 defined('_JEXEC') or die;
-print_r($tables);
-
 ?>
-
-<table width="100%">
+<?php echo $modbefore; ?>
+<table<?php echo $tableclass; ?>>
     <thead>
         <tr>
-        <?php
+        <?php $ncols = 0;
         foreach($table[0] AS $key => $item){
-                echo '<td>'.getHeader($key).'</td>';
+                echo '<th>'. $st->getHeader( &$params ,$key ) .'</th>';
+                $ncols++;
         }
         ?> 
         </tr>
 </thead>
     <tbody>
-        <?php $i=0;
+        <?php $i=0; $rowprefix='';
         foreach($table AS $row){
-        $k = $i++ % 2;
-        echo '<tr class="row'. $k .'">';
-                foreach($row AS $key => $item){
-                        echo '<td>'.$item.'</td>';
-                }
-        echo '</tr>';
+            if($tablerowprefix != ''){
+                $k = $i++ % 2;
+                $rowprefix = ' class="'. $tablerowprefix . $k .'"';
+            }
+            echo '<tr'.$rowprefix.'>';
+                    foreach($row AS $key => $item){                        
+                        echo '<td>'.$st->addLink(&$params , $row, $key, $item).'</td>';
+                    }
+            echo '</tr>';
         }
         ?>        
     </tbody>
-    <tfoot><tr><?php echo count($table); ?></tr></tfoot>
+    <tfoot>
+        <tr><td colspan="<?php echo $ncols; ?>"><?php echo $nitems; ?></td></tr></tfoot>
 </table>
-
-
-<?php 
-echo '<pre>Table:';
-print_r($table);
-echo '</pre>';
-    print_r($params);
-if( $params->get('debug',0) ){
-    echo '<pre> Table:';
-    print_r($table);
-    echo '<hr /> Params';
-    print_r($params);
-    echo '<hr /> Response';
-    print_r($response);
-    echo '</pre>';
-}
-?>
+<?php echo $modafter; ?>
